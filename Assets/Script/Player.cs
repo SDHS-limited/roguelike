fd4c19218bd9a49f26bb54e0156c1fcd6c79a330
SDHS-limited <wooozem@gmail.com>
2026-01-05 14:54:30 +0900

    던전 생성 알고리즘 추가

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float speed = 5.0f;
+    Rigidbody rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveH = Input.GetAxisRaw("Horizontal");
+        float moveV = Input.GetAxisRaw("Vertical");
+        Vector3 move = new Vector3(moveH, 0, moveV);
+        transform.Translate(move * speed * Time.deltaTime, Space.World);
+    }
+}
