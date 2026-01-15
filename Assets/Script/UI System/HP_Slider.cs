09748b515becf368f132bafe545b5e7c6f863568
limited <wooozem@gmail.com>
2026-01-15 09:37:12 +0900

    HP시스템 수정

--- Assets/Script/UI System/HP_Slider.cs
+++ Assets/Script/UI System/HP_Slider.cs
@@ -1,16 +1,29 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HP_Slider : MonoBehaviour
 {
+    [SerializeField] Slider HPslider;
+    public float maxHp = 100;
+    public float curHP = 100;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        HPslider.value = curHP / maxHp;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // if (Input.GetKeyDown(KeyCode.Backspace))
+        // {
+        //     curHP -= 10;
+        // }
+        // if(curHP >= 0)
+        // {
+        //     Debug.Log("기절");
+        // }
+
+        HPslider.value = curHP / maxHp;
     }
 }
